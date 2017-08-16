5bc69a826a1149203de4e2254e31266dfd3f5f9b
lishiqiang <565333036@@qq.com>
2017-08-16 22:44:40 +0800

    1、格式化代码 2、webpack命令改为局部

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import {Link, Route, Redirect, Switch} from "react-router-dom";
 
-
-// import Bundle from '../components/Bundle/bundle'
 import {DialogLayout} from '../components/Dialog'
 import Bundle from './bundle.js';
-// var Home = require("bundle-loader?lazy&name=home!./home/home.js");
 import Home from 'bundle-loader?lazy&name=home!./home/home.js';
 const List = () => (
     <Bundle load={Home}>
