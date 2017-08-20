947bf42c19f8448200e156372a7752017ce76034
lishiqiang <565333036@@qq.com>
2017-08-21 00:37:50 +0800

    增加Redirect重定向

--- src/router/home/home.js
+++ src/router/home/home.js
@@ -6,13 +6,23 @@ import React, {
 import {
   connect
 } from 'react-redux'
+import {DialogLayout} from '../../components/Dialog'
+import {Link} from "react-router-dom";
 function mapStateToProps(state){
 	return state
 }
 class Home extends Component{
 	
 	render(){
-		return <h1>首页</h1>
+		return (
+			<div>
+				<h1>首页</h1>
+				<Link to='/home1'>首页1</Link>
+				<Link to='/home2'>首页2</Link>
+				<DialogLayout />
+			</div>
+		)
+		
 	}
 }
 const _Home = connect(mapStateToProps)(Home)
--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,20 @@ import {DialogLayout} from '../components/Dialog'
 import Bundle from '../components/Bundle/bundle.js'
 import * as utils from '../utils/util'
 import Home from 'bundle-loader?lazy&name=home!./home/home.js';
+import Home1 from 'bundle-loader?lazy&name=home!./home1/home.js';
+import Home2 from 'bundle-loader?lazy&name=home!./home2/home.js';
 
 class App extends Component {
 	render(){
 		return (
-			<div className='asdfas111'>
-				<Route path="/" component={DialogLayout} />
-				<Route path="/home" component={utils.syncBundle(Home)} />
+			<div className='content'>
+				
+				<Switch>
+					<Route path="/home" component={utils.syncBundle(Home)} />
+					<Route path="/home1" component={utils.syncBundle(Home1)} />
+					<Route path="/home2" component={utils.syncBundle(Home2)} />
+					<Redirect exact from="/" to="/home"></Redirect>
+				</Switch>
 			</div>
 		)
 	}
