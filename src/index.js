ee86fa5d9f4f5382f4d093fba1019d96b6ffdb05
lishiqiang <565333036@@qq.com>
2017-08-23 00:17:50 +0800

    add history.listen

--- src/index.js
+++ src/index.js
@@ -14,21 +14,25 @@ import * as _router from 'react-router-dom'
 import App from './router/index'
 
 import { Provider } from 'react-redux';
-// import { createHistory } from 'history'
+// import * as _history from 'history'
+import { createBrowserHistory } from 'history'
 
 import store from './store'
-console.log('store',store)
-console.log('browserHistory',browserHistory)
-console.log('_router',_router)
-console.log('createHistory',createHistory)
+// console.log('store',store)
+// console.log('browserHistory',browserHistory)
+// console.log('_router',_router)
+console.log('createBrowserHistory',createBrowserHistory)
 
-// const history = createHistory()
-
+const history = createBrowserHistory()
+console.log('history',history)
+history.listen(function(location){
+	console.log('listen',location)
+})
 render(
 	<Provider store={store}>
-     <BrowserRouter>
-      <Route path="/" component={App} />
-    </BrowserRouter>
+     	<Router history={history}>
+      	<Route path="/" component={App} />
+      </Router>
   </Provider>,
 	document.getElementById('root')
 )
