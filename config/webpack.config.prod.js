c4e24fecca020d43932ea9242a270445acacd255
lishiqiang <565333036@@qq.com>
2017-12-11 02:31:25 +0800

    优化config配置

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,11 +4,8 @@ var htmlWebpackPlugin = require("html-webpack-plugin");
 // 项目依赖图显示
 var Visualizer = require('webpack-visualizer-plugin');
 
-let  {
-  moduleWebpack,
-  resolveWebpack,
-  devServerWebpack
-} = require('./webpack.config.common.js')
+
+var config = require("./blockConfig");
 
 module.exports = {
   cache: true, //开启缓存模式
@@ -19,72 +16,7 @@ module.exports = {
     chunkFilename: '[name].[chunkhash:5].chunk.js',
     publicPath: '/'
   },
-  module: moduleWebpack,
-  devtool: 'cheap-source-map',
-  devServer: devServerWebpack,
-  resolve: resolveWebpack,
-  plugins: [
-    // 项目依赖图
-    new Visualizer({
-      filename: '../../log/statistics.html'
-    }),
-    new htmlWebpackPlugin({
-      title: "搭建前端工作流",
-      template: "./src/index.html"
-    })
-  ]
+  module: config.moduleConfig,
+  resolve: config.resolveConfig,
+  plugins: config.prodPluginConfig
 }
-
-
-// var path = require('path')
-// var webpack = require('webpack')
-// var htmlWebpackPlugin = require("html-webpack-plugin");
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, "dist"), // string
-//     filename: 'bundle.js',
-//     chunkFilename: '[name].[chunkhash:5].chunk.js',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /(node_modules|bower_components)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['env']
-//           }
-//         }
-//       },
-//       {
-//         test: /\.(png|jpg|gif)$/,
-//         use:[{
-//           loader: "url-loader?limit=1000"
-//         }]
-//       },
-//       {
-//         test: /\.(eot|svg|ttf|woff|woff2)\w*/,
-//         use: [{
-//           loader: "file-loader"
-//         }]
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [
-//           'style-loader',
-//           'css-loader',
-//           'less-loader'
-//         ]
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new htmlWebpackPlugin({
-//       title: "搭建前端工作流",
-//       template: "./src/index.html"
-//     })
-//   ]
-// }
