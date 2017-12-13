c6bffae91e7064fa2a504e9ff1633c1b8350dd06
李士强 <lishiqiang@didichuxing.com>
2017-12-13 18:11:29 +0800

    注释无用代码

--- src/router/home/store.js
+++ src/router/home/store.js
@@ -3,10 +3,10 @@ import { observable, action } from 'mobx';
 const homeStore = observable({
   count: 1,
   add: action.bound(function fn() {
-  	this.count += 1;
+    this.count += 1;
   }),
   less: action.bound(function fn() {
-  	this.count -= 1;
+    this.count -= 1;
   }),
 });
 export default homeStore;
--- src/store/location.js
+++ src/store/location.js
@@ -1,29 +1,29 @@
-// ------------------------------------
-// Constants
-// ------------------------------------
-export const LOCATION_CHANGE = 'LOCATION_CHANGE';
+// // ------------------------------------
+// // Constants
+// // ------------------------------------
+// export const LOCATION_CHANGE = 'LOCATION_CHANGE';
 
-// ------------------------------------
-// Actions
-// ------------------------------------
-export function locationChange(location = '/') {
-  return {
-    type: LOCATION_CHANGE,
-    payload: location,
-  };
-}
+// // ------------------------------------
+// // Actions
+// // ------------------------------------
+// export function locationChange(location = '/') {
+//   return {
+//     type: LOCATION_CHANGE,
+//     payload: location,
+//   };
+// }
 
-// ------------------------------------
-// Specialized Action Creator
-// ------------------------------------
-export const updateLocation = ({ dispatch }) => nextLocation => dispatch(locationChange(nextLocation));
+// // ------------------------------------
+// // Specialized Action Creator
+// // ------------------------------------
+// export const updateLocation = ({ dispatch }) => nextLocation => dispatch(locationChange(nextLocation));
 
-// ------------------------------------
-// Reducer
-// ------------------------------------
-const initialState = null;
-export default function locationReducer(state = initialState, action) {
-  return action.type === LOCATION_CHANGE
-    ? action.payload
-    : state;
-}
+// // ------------------------------------
+// // Reducer
+// // ------------------------------------
+// const initialState = null;
+// export default function locationReducer(state = initialState, action) {
+//   return action.type === LOCATION_CHANGE
+//     ? action.payload
+//     : state;
+// }
