b00f19596e3b46a97567d8dea32c31e6261c7cb1
李士强 <lishiqiang@didichuxing.com>
2017-12-13 16:46:31 +0800

    add readme.md

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    // "extends": "airbnb",
-    // "parser": "babel-eslint",
-    // globals: {
-    //   sessionStorage: false,
-    //   document: false,
-    //   window: false,
-    //   Headers: false,
-    //   rootPath: false,
-    //   document: false,
-    //   Swiper: false,
-    //   DDMap: false,
-    //   Raven: false,
-    // },
-    // rules: {
-    //   'camelcase': 0,
-    //   'react/prop-types': 0,
-    //   'import/extensions': 0,
-    //   'import/no-unresolved': 0,
-    //   'react/no-array-index-key': 0,
-    //   'import/no-extraneous-dependencies': 0
-    // }
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    globals: {
+      sessionStorage: false,
+      document: false,
+      window: false,
+      Headers: false,
+      rootPath: false,
+      document: false,
+      Swiper: false,
+      DDMap: false,
+      Raven: false,
+    },
+    rules: {
+      'camelcase': 0,
+      'react/prop-types': 0,
+      'import/extensions': 0,
+      'import/no-unresolved': 0,
+      'react/no-array-index-key': 0,
+      'import/no-extraneous-dependencies': 0
+    }
 };
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 // 编译代码
   npm run build 
 
-// 编译公共组建
+// 编译公共组件
   npm run buildlib
 ```
 # 请将dist.zip 解压 并替换 node_modules/jimu-mobile/dist 里面的所有文件
